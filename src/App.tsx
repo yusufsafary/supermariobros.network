1e32d4a222ff3126a1b2e43a2e64c46ef8ad3f89
arthouse <146998950+yusufsafary@users.noreply.github.com>
2026-06-22 15:32:53 +0700

    Add /play route, Play page has no Navbar

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import { Router, Route, Switch } from "wouter";
 import { useHashLocation } from "wouter/use-hash-location";
 import Home from "@/pages/Home";
+import Play from "@/pages/Play";
 import About from "@/pages/About";
 import HowToPlay from "@/pages/HowToPlay";
 import CookiePolicy from "@/pages/CookiePolicy";
@@ -16,15 +17,22 @@ function App() {
   return (
     <Router hook={useHashLocation}>
       <TooltipProvider>
-        <Navbar />
         <Switch>
-          <Route path="/" component={Home} />
-          <Route path="/about" component={About} />
-          <Route path="/how-to-play" component={HowToPlay} />
-          <Route path="/cookie-policy" component={CookiePolicy} />
-          <Route path="/privacy-policy" component={PrivacyPolicy} />
-          <Route path="/terms" component={Terms} />
-          <Route component={NotFound} />
+          <Route path="/play" component={Play} />
+          <Route>
+            <>
+              <Navbar />
+              <Switch>
+                <Route path="/" component={Home} />
+                <Route path="/about" component={About} />
+                <Route path="/how-to-play" component={HowToPlay} />
+                <Route path="/cookie-policy" component={CookiePolicy} />
+                <Route path="/privacy-policy" component={PrivacyPolicy} />
+                <Route path="/terms" component={Terms} />
+                <Route component={NotFound} />
+              </Switch>
+            </>
+          </Route>
         </Switch>
         <Toaster />
       </TooltipProvider>
